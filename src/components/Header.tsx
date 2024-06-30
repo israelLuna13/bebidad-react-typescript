3287a9a3426bb5f8a4aa27a10c74fac5237014d8
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-30 16:03:41 -0600

    Se agrego nevegacion entre paginas, resaltando pagina actual, mas informacion de la pagina actual y se creo el formulario de busqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,66 @@
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 export default function Header() {
+  const { pathname } = useLocation();
+
+  const isHome = useMemo(() => pathname === "/", [pathname]);
   return (
-    <header className="bg-slate-800">
+    <header className={isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
-            <img src="/logo.svg" alt="logotipo" className="w-32"/>
+            <img src="/logo.svg" alt="logotipo" className="w-32" />
           </div>
 
-          <nav></nav>
+          <nav className="flex gap-4">
+            {/* con navLink detectamos cual vista esta activa  */}
+            <NavLink  to="/" className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : " text-white uppercase font-bold"} >  Inicio </NavLink>
+            <NavLink to="/favoritos"  className={({ isActive }) => isActive ? "text-orange-700 uppercase font-bold" : "text-white uppercase font-bold" }> Favoritos </NavLink>
+          </nav>
         </div>
+
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o ingredientes
+              </label>
+
+              <input
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Nombre o ingrediante. Ej. Vodka. Tequila"
+              />
+            </div>
+
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoria
+              </label>
+
+              <select
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+              >
+                <option value="">--Seleccione--</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Buscar recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
